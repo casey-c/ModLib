9a02170bfaffb478c5acfe2470478a9b0a4a6825
casey <406326-casey@users.noreply.gitlab.com>
2020-09-07 15:57:26 -0400

    Nested Horiz/Vert layouts working

--- src/main/java/testing/TestScreen.java
+++ src/main/java/testing/TestScreen.java
@@ -12,6 +12,10 @@ import ui.widgets.labels.SimpleLabel;
 
 public class TestScreen extends LargeScreen<VerticalLayout> {
     public TestScreen() {
+        //nestedLayoutTest();
+    }
+
+    private void nestedLayoutTest() {
         mainLayout = new VerticalLayout(
                 CONTENT_MIN_X,
                 CONTENT_MIN_Y,
@@ -20,9 +24,22 @@ public class TestScreen extends LargeScreen<VerticalLayout> {
                 40.0f
         );
 
+        HorizontalLayout innerLayout = new HorizontalLayout(
+                CONTENT_MIN_X,
+                CONTENT_MIN_Y,
+                SCREEN_W - 2.0f * CONTENT_OUTER_PADDING_X,
+                80.0f,
+                40.0f
+        );
+
         mainLayout.addChild(new SimpleLabel("Test Screen", FontHelper.bannerFont, Settings.GOLD_COLOR));
         mainLayout.addChild(new SimpleLabel("Hello, world", FontHelper.tipBodyFont, Settings.CREAM_COLOR));
-        mainLayout.addChild(new SimpleLabel("More Text", FontHelper.tipBodyFont, Settings.RED_TEXT_COLOR));
-        mainLayout.addChild(new SimpleLabel("YESSSSSS", FontHelper.tipBodyFont, Settings.BLUE_TEXT_COLOR));
+        mainLayout.addChild(SimplePadding.vertical(20.0f));
+
+        mainLayout.addChild(innerLayout);
+        innerLayout.addChild(new SimpleLabel("More Text", FontHelper.tipBodyFont, Settings.RED_TEXT_COLOR));
+        innerLayout.addChild(new SimpleLabel("YESSSSSS", FontHelper.tipBodyFont, Settings.BLUE_TEXT_COLOR));
+
+        mainLayout.addChild(new SimpleLabel("Hello, world", FontHelper.tipBodyFont, Settings.CREAM_COLOR));
     }
 }
